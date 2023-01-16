370e8d8b326b4b39e2b54f7c893634c4c604d152
PiotrO9 <piotr.ostrowski02@wp.pl>
2023-01-16 17:23:44 +0100

    Removed files from basic template, add  views, components and styles

---
+++ src/views/DetailView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="DetailView">
+    <h1>{{ maintext }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            maintext: ""
+        }
+    },
+    mounted() {
+        this.maintext = this.$route.params.id
+    }
+
+}
+</script>
+
+<style lang="scss">
+@import "../assets/Styles/ViewsStyles/DetailViewStyles.scss";
+</style>
