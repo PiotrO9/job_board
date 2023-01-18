3666f1dd800f571b9fae8801302e014570f4d79e
PiotrO9 <piotr.ostrowski02@wp.pl>
2023-01-18 14:22:06 +0100

    Add features for UserDatas components like animations, modal dropdown datas

--- src/components/UserDatas.vue
+++ src/components/UserDatas.vue
@@ -1,9 +1,35 @@
 <template>
-  <div class="UserDatas">
-    <img class="PictureData" :src=PictureURL>
-    <p class="NameData">{{ Name }}</p>
-    <img class="down-arrow" src="../assets/images/down-arrow.png">
+  <div v-if="UserDatas != undefined" class="UserDatas" @click="ArrowClick">
+    <img class="PictureData" :src=UserDatas.picture.thumbnail alt="Profile picture">
+    <p class="NameData">{{ UserDatas.name.first }}</p>
+    <img id="UserDataDownArrow" class="down-arrow" src="../assets/images/down-arrow.png"> 
+    <transition appear v-if="ModalUserDataVisibility" name="ModalUserData">
+        <div class="modalUserDatas">
+            <div class="GeneralDatas">
+                <img :src=UserDatas.picture.thumbnail alt="Profile picture">
+                <div class="BasicDatas">
+                    <p>{{ UserDatas.name.first }}  {{ UserDatas.name.second }}</p>
+                    <p>
+                        <fa icon="fa-location-dot"/>
+                        <span id="LocationDatas">{{ UserDatas.location.country }}, {{ UserDatas.location.city }}</span>
+                    </p>
+                </div>
+            </div>
+            <hr>
+            <div class="StatisticsFields">
+                <div class="PostField">
+                    <p>Posts</p>
 
+                </div>
+                <div class="FollowersField">
+
+                </div>
+                <div class="FollowingField">
+
+                </div>
+            </div>
+        </div>
+    </transition>
   </div>
 </template>
 
@@ -14,16 +40,56 @@ export default {
     data() {
         return {
             Name: "",
-            PictureURL: ""
+            PictureURL: "",
+            IsArrowDirectionDown: true,
+            ModalUserDataVisibility: false,
+            UserDatas: undefined
         }
     },
     mounted() {
         FetchRandomUserApi()
         .then((res) => {
-            this.Name = res.name.first
-            this.PictureURL = res.picture.thumbnail
-            console.log(res)
+            this.UserDatas = res 
         })
+
+    },
+    methods: {
+        ArrowClick() {
+            const imageWithArrow = document.getElementById("UserDataDownArrow")
+            const UserDatas = document.querySelector(".UserDatas");
+
+            if(this.IsArrowDirectionDown) {
+                imageWithArrow.classList.remove("rotateArrowDown")
+                UserDatas.classList.remove("moveUserDataToLeft")
+                imageWithArrow.classList.add("rotateArrowUp")
+                UserDatas.classList.add("moveUserDataToRight")
+            }
+            else {
+                imageWithArrow.classList.remove("rotateArrowUp")
+                UserDatas.classList.remove("moveUserDataToRight")
+                imageWithArrow.classList.add("rotateArrowDown")
+                UserDatas.classList.add("moveUserDataToLeft")
+            }
+            this.IsArrowDirectionDown = !this.IsArrowDirectionDown
+            this.ModalUserDataVisibility = !this.ModalUserDataVisibility
+
+            
+        }
+    },
+    watch: {
+        ModalUserDataVisibility() {
+            setTimeout(() => {
+                if(this.ModalUserDataVisibility) {
+                    let CountryLength = this.UserDatas.location.country.length
+                    let CityLength = this.UserDatas.location.city.length
+                    
+                    if(CountryLength + CityLength > 20) {
+                        const LocationDatas = document.getElementById("LocationDatas")
+                        LocationDatas.classList.add("SmallerLocationFont")
+                    }
+                }
+            }, 1000)
+        }
     }
 }
 </script>
